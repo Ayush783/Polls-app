33ab29958340059ddfefed203d77800d2e80ccd8
Ayush783 <ayushc45xxx@gmail.com>
2021-05-17 01:22:43 +0530

    created details results and vote views

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Index page")
+
+def details(request, question_id):
+    return HttpResponse("details page")
+
+def results(request, question_id):
+    return HttpResponse("results page")
+
+def vote(request, question_id):
+    return HttpResponse("votes action")
