0fd182de97b9cbd18e70ae1a1efe28de9d2846c6
Ayush783 <ayushc45xxx@gmail.com>
2021-05-17 13:02:57 +0530

    completed vote action

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse 
-from .models import Question
+from django.http import HttpResponse ,HttpResponseRedirect
+from .models import Choice, Question
+from django.urls import reverse
 # Create your views here.
 def index(request):
     polls_list = Question.objects.all().order_by('pub_date')
@@ -15,4 +16,15 @@ def results(request, question_id):
     return HttpResponse("results page")
 
 def vote(request, question_id):
-    return HttpResponse("votes action")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
