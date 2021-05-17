93842922d78d6982f9af8dd88bde8b081856a19c
Ayush783 <ayushc45xxx@gmail.com>
2021-05-17 13:08:55 +0530

    all functionality of polls app complete

--- polls/views.py
+++ polls/views.py
@@ -13,7 +13,8 @@ def details(request, question_id):
     return render(request,'polls/details.html',{'question':question})
 
 def results(request, question_id):
-    return HttpResponse("results page")
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/result.html',{'question':question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
