85434635cfec9b502c54585f43719f57c754072e
Ayush783 <ayushc45xxx@gmail.com>
2021-05-23 22:50:52 +0530

    registered choices in question admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['question_text','pub_date']
+    inlines = [ChoiceInline]
+
+admin.site.register(Question,QuestionAdmin)
