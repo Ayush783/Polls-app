ff4358ab7d8f715cee4d48d722530e45e8a0350c
Ayush783 <ayushc45xxx@gmail.com>
2021-05-24 17:43:55 +0530

    finished create poll

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
+from django.http.response import Http404, HttpResponseBadRequest, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from .models import Choice, Question
 from django.db.models import F
 from django.urls import reverse
+from django.utils import timezone
+
 # Create your views here.
 def index(request):
     polls_list = Question.objects.all().order_by('pub_date')
@@ -34,4 +37,26 @@ def recent(request):
     return render(request,'polls/recent-polls.html',{'polls_list':polls_list})
 
 def create(request):
+    if request.method == "POST":
+        question_text = request.POST.get('question_text')
+        choice1 = request.POST.get('choice1')
+        choice2 = request.POST.get('choice2')
+        choice3 = request.POST.get('choice3')
+        choice4 = request.POST.get('choice4')
+        q = Question.objects.create(question_text=question_text,pub_date=timezone.now())
+        q.choice_set.create(choice_text = choice1,votes = 0)
+        q.choice_set.create(choice_text = choice2,votes = 0)
+        q.choice_set.create(choice_text = choice3,votes = 0)
+        q.choice_set.create(choice_text = choice4,votes = 0)
+        try:
+            return JsonResponse({
+                "status" : 200,
+                "data":{
+                    "question_id":q.id,
+                    "question_text":q.question_text,
+                    "choices":[choice1,choice2,choice3,choice4]
+                }
+            })
+        except:
+            return Http404()
     return render(request,'polls/create-poll.html')
